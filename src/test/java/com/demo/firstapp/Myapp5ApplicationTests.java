40933f9d2a253277cf57839df3cd394954e04cc6
Anjali-Damodaram <104523058+Anjali-Damodaram@users.noreply.github.com>
2022-05-09 13:14:49 +0530

    Update Myapp5ApplicationTests.java

--- src/test/java/com/demo/firstapp/Myapp5ApplicationTests.java
+++ src/test/java/com/demo/firstapp/Myapp5ApplicationTests.java
@@ -1,16 +1,22 @@
 package com.demo.firstapp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-
+@ContextConfiguration
 class Myapp5ApplicationTests {
-
-	private Myapp5Application controller;
-@Test
+	@InjectMocks
+private Myapp5Application controller;
+/*@Test
 public void contextLoads() throws Exception{
 	Myapp5Application.main(new String[] {});
+}*/
+@Test
+public void Test(){
+controller.main(new String[] {});
 }
-
-
-}
+}		 
