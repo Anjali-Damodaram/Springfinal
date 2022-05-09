a70577ec375d5ccf776b64dc3d6c8a56ca7e8813
Anjali-Damodaram <104523058+Anjali-Damodaram@users.noreply.github.com>
2022-05-09 12:59:43 +0530

    Update Myapp5ApplicationTests.java

--- src/test/java/com/demo/firstapp/Myapp5ApplicationTests.java
+++ src/test/java/com/demo/firstapp/Myapp5ApplicationTests.java
@@ -3,7 +3,7 @@ package com.demo.firstapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class Myapp5ApplicationTests {
 
 	private Myapp5Application controller;
